216e4660a0d983de94beeaa2b77fdb3ffe2d6313
Bruttix <bgreene212@gmail.com>
2022-02-22 22:10:54 -0600

    frontend client facing
    update 0.0.2

---
+++ FashionShop/client frontend/README.md
@@ -0,0 +1,6 @@
+/* Client Side facing configuration */
+1. Look for any existing package-lock.json or /node_modules/ and DELETE them (hint: /node_modules/ made be hidden directory, so enable view all files).
+2. Open a developer powershell terminal, change each terminal into the current project and then /client frontend/ 
+   then type the commands below into the terminal which will update the projects node_modules and package-lock.json
+    -  npm install
+    -  npm start
---
+++ FashionShop/client frontend/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us Monday-Saturday </h5>
+                      <p>8 a.m - 6 p.m</p>
+                      <p>864 - 275 - 6969</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Headquarters</h5>
+            <p>San Antonio, TX</p>
+            <p>United States</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>Fax</h5>
+            <p>0420 232 0420</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
